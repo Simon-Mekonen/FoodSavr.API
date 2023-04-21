c4f0c0608345f6fb8a856ad8dba53f5a3f02d157
Simon Mekonen <simon.mekonen@b3.se>
2023-04-21 14:04:21 +0200

    module 3 update

--- FoodSavr.API/Controllers/FoodSavrController.cs
+++ FoodSavr.API/Controllers/FoodSavrController.cs
@@ -2,21 +2,39 @@
 using Microsoft.AspNetCore.Mvc;
 namespace FoodSavr.API
 {
-    //
     [ApiController]
     [Route("api/foodsavr")]
     public class FoodSavrController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetIngredients() 
+        public ActionResult<IEnumerable<IngredientDataStore>> GetIngredients() 
         {
-            return new JsonResult(IngredientDataStore.Current.Ingredients);
+            var ingredientsToReturn = IngredientDataStore.Current.Ingredients;
+
+            if (ingredientsToReturn == null)
+                return NotFound();
+
+            return Ok (ingredientsToReturn);
         }
         [HttpGet("{id}")]
-        public JsonResult GetIngredient(int id)
+        public ActionResult<IngredientDto> GetIngredient(int id)
         {
-            return new JsonResult(
-                IngredientDataStore.Current.Ingredients.FirstOrDefault(i => i.Id == id));
+            var ingredientToReturn = IngredientDataStore.Current.Ingredients.FirstOrDefault(i => i.Id == id);
+            if (ingredientToReturn == null) 
+                return NotFound();
+            
+            return Ok (ingredientToReturn);
         }
+        [HttpGet("ingredientcount")]
+        public ActionResult<int> GetIngredientCount()
+        {
+            int ingredientCount = IngredientDataStore.Current.NumberOfIngredients;
+            if (ingredientCount == 0)
+            {
+                return NotFound();
+            }
+            return Ok(ingredientCount);
+        }
+
     }
 }
