79a013c67feac202cad2c2c6c60e0358e4f4a1c2
Simon Mekonen <simon.mekonen@b3.se>
2023-04-21 10:51:45 +0200

    Adding files

---
+++ FoodSavr.API/Controllers/FoodSavrController.cs
@@ -0,0 +1,22 @@
+﻿using FoodSavr.API.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace FoodSavr.API
+{
+    //
+    [ApiController]
+    [Route("api/foodsavr")]
+    public class FoodSavrController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetIngredients() 
+        {
+            return new JsonResult(IngredientDataStore.Current.Ingredients);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetIngredient(int id)
+        {
+            return new JsonResult(
+                IngredientDataStore.Current.Ingredients.FirstOrDefault(i => i.Id == id));
+        }
+    }
+}
---
+++ FoodSavr.API/Models/IngredientDataStore.cs
@@ -0,0 +1,46 @@
+﻿namespace FoodSavr.API.Models
+{
+    public class IngredientDataStore
+    {
+        public List<IngredientDto> Ingredients { get; set; }
+        public static IngredientDataStore Current { get; } = new IngredientDataStore();
+        public IngredientDataStore() 
+        {
+            // init dummy data
+            Ingredients = new List<IngredientDto>()
+            {
+                new IngredientDto()
+                {
+                    Id = 1,
+                    Name = "Tomat"
+                },
+                new IngredientDto()
+                {
+                    Id = 2,
+                    Name = "Fisk"
+                },
+                new IngredientDto()
+                {
+                    Id = 3,
+                    Name = "Nötkött"
+                },
+                new IngredientDto()
+                {
+                    Id = 4,
+                    Name = "Sallad"
+                },
+                new IngredientDto()
+                {
+                    Id = 5,
+                    Name = "Persilja"
+                },
+                new IngredientDto()
+                {
+                    Id = 6,
+                    Name = "Avokado"
+                },
+            };
+        }
+    }
+
+}
